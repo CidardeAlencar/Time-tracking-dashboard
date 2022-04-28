1b5407cff8b22a534067397a6428af9a78ceafac
Cidar de Alencar <cidardealencar@LAPTOP-FGLAEUQ5.localdomain>
2022-04-28 10:14:11 -0400

    [add] finalizacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Profile from "./components/Profile";
+import Dashboard from "./components/Dashboard";
+import Posts from "./data/data.json";
+import Footer from "./components/Footer";
 
 function App() {
+  const [time, setTime] = useState("Weekly");
+
+  const handleClick = (e) => {
+    if (e.target.innerHTML == "Daily") {
+      setTime("Daily");
+    } else if (e.target.innerHTML == "Weekly") {
+      setTime("Weekly");
+    } else if (e.target.innerHTML == "Monthly") {
+      setTime("Monthly");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main className="w-[326px] my-36 lg:w-3/4  mx-auto   lg:grid  lg:grid-cols-4 lg:grid-cols-[repeat(4,_minmax(260px,_1fr))]d  lg:grid-rows-2  gap-6">
+        <Profile handleClick={handleClick} time={time} />
+        <Dashboard time={time} Posts={Posts} />
+        <Footer />
+      </main>
+    </>
   );
 }
 
